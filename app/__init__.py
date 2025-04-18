ba982f9cdac1839f5c97c29f78b3abe67f8ffbf4
taqerun <zhaxer1407@gmail.com>
2025-04-19 02:54:29 +0300

    Replaces `get_bot_token` with `BOT_TOKEN`
    Simplifies bot token retrieval by directly referencing
    the `BOT_TOKEN` constant instead of using a getter function.
    This improves code readability and efficiency.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from app.core.bot import start_bot
-from app.core.config import redis, i18n, get_bot_token, DB_URI
+from app.core.config import redis, i18n, BOT_TOKEN, DB_URI
 
 
-__all__ = ["start_bot", "redis", "i18n", "get_bot_token", "DB_URI"]
+__all__ = ["start_bot", "redis", "i18n", "BOT_TOKEN", "DB_URI"]
