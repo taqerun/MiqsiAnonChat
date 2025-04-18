6d944641246feb3ea0b0ba84fb18c617397ddc3a
taqerun <zhaxer1407@gmail.com>
2025-04-19 02:11:47 +0300

    Removes dotenv loading from config
    Removes the loading of environment variables using
    the `dotenv` library from the configuration file.
    
    This change assumes that environment variables are now
    handled by the system or another dedicated mechanism,
    simplifying the configuration process.

--- app/core/config.py
+++ app/core/config.py
@@ -2,12 +2,8 @@ from os import getenv
 from pathlib import Path
 
 from aiogram.utils.i18n import I18n
-from dotenv import load_dotenv
 from redis.asyncio import Redis
 
-
-# === Load environment variables ===
-load_dotenv()
 
 # === Paths ===
 BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent
